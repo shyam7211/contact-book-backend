de6938fefcb8749899cfe201789cd620f967f091
Shyam Sunder Prajapat <86419377+shyam7211@users.noreply.github.com>
2023-08-15 18:03:49 +0530

    Update server.js
    For deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { errorHandler } = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config()
 
-connectDb();
+
 const app = express();
 const port = process.env.PORT || 6000;
 
@@ -12,6 +12,8 @@ app.use('/api/contacts',require('./routes/contactRoutes'));
 app.use('/api/user',require('./routes/userRoutes'));
 app.use(errorHandler);
 
-app.listen(port,() => {
-    console.log("Hello there i am in " + port);
+connectDb().then(() => {
+    app.listen(port, () => {
+        console.log("Hello there i am in " + port);
+    })
 })
